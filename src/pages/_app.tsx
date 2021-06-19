d7e177266668a589f0a6673450e73490d6346f0e
sleduardo20 <sl.eduardo20@gmail.com>
2021-06-18 22:07:40 -0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,17 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import GlobalStyles from 'styles/GlobalStyles';
+import './styles.css';
+import { AuthProvider } from '../hooks/useAuth';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AuthProvider>
       <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work TypeScript, React,NextJS and Styled Components"
-        />
+        <title>Autenticação em Next.js</title>
       </Head>
       <Component {...pageProps} />
-      <GlobalStyles />
-    </>
+    </AuthProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,47 @@
-import { Main } from 'components/Main';
+import { useAuth } from 'hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { withSSRGuest } from 'utils/withSSRGuest';
 
-const Home: React.FC = () => {
-  return <Main />;
+const Home = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useAuth();
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </form>
+  );
 };
 
 export default Home;
+
+export const getServerSideProps = withSSRGuest(async ctx => {
+  return {
+    props: {},
+  };
+});
